6d5873e5ed216741db7d271832373e0943369aa5
J JITESH REDDY <79923685+jiteshjampa@users.noreply.github.com>
2025-02-11 10:25:01 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,7 +5,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/jiteshjampa",
   resume:
-    "https://drive.google.com/file/d/1mPPzf07B5o8pUDnN8Y5utZGB4K0Ri3a0/view?usp=sharing", 
+    "https://drive.google.com/file/d/1oB-ljxT1iiXmovfvS5KfVx9HHlh-92WA/view?usp=sharing", 
   linkedin: "https://www.linkedin.com/in/jitesh-jampa-4b2701199/",
 
   facebook: "https://www.facebook.com/jiteshreddy.jampa",
